96f4a5fe73e020e6989a3a2234dc0b4375b28f4a
Narsi <154037409+Narsi7777@users.noreply.github.com>
2026-07-09 09:39:57 +0530

    Change packet quantity text color based on value

--- Client/src/components/StorageList.js
+++ Client/src/components/StorageList.js
@@ -173,7 +173,14 @@ const StorageList=()=>{
                 {sortedStorage.map((item,index)=>(
                     <div className="storage-box" key={index}>
                         <h2>{item.nameofthebrand}</h2>
-                        <p>Packets:{item.quantityinpackets}</p>
+                        <p
+  style={{
+    color: item.quantityinpackets <= 0 ? "red" : "black",
+    fontWeight: item.quantityinpackets <= 0 ? "bold" : "normal",
+  }}
+>
+  Packets: {item.quantityinpackets}
+</p>
                         <p>Cost:{item.costofeachpacket}/-</p>
                         <div className="button-container">
                             {buttonClick&&<div>
